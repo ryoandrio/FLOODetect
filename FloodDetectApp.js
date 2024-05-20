9f7f32d12808fcff5bda7da17cbf117d518a6f1d
ryoandrio <ryoandrio06@gmail.com>
2024-05-20 20:25:28 +0700

    floodAreaMapping function - draft unfinished

--- FloodDetectApp.js
+++ FloodDetectApp.js
@@ -497,7 +497,7 @@
       label: "Draw Region of Interest (ROI)",
       onClick: function() {
         drawROIFunction();
-        // setGeometryROI();
+        setGeometryROI();
       },
       style: { stretch: "horizontal", color: "black" },
     });
@@ -628,7 +628,18 @@
     // Define the Region of Interest (ROI) for the analysis
 
 
-    // Flood Mapping Algorithm
+    // Clip Imagery Function
+    function clipImagery() {
+      if(geometryImage) {
+        //call Set Parameter Function
+        setParams();
+
+        //Convert the date objects to YYYY-MM-DD format
+
+      }
+    }
+
+    // Flood Mapping Algorithm Function
     function floodAreaMapping() {
       var geom = drawingTools.layers().get(0).toGeometry();
       if (geom) {
@@ -662,10 +673,86 @@
         .filter(ee.Filter.listContains('transmitterReceiverPolarisation', polarization))
         .filter(ee.Filter.eq('orbitProperties_pass', pass_direction)) 
         .filter(ee.Filter.eq('resolution_meters', 10))
-        // .filterBounds(bounds)
+        .filterBounds(geometry)
         .select(polarization)
 
+      var before = collection.filterDate(before_start, before_end);
+      var after = collection.filterDate(after_start, after_end);
 
+      //Generating Reference and Flood Multi-temporal SAR Data
+      //Mean Before and Min After
+      var mean_before = before.mean().clip(bounds)
+      var min_after = after.min().clip(bounds)
+      var max_after = after.max().clip(bounds)
+      var mean_after = after.mean().clip(bounds)
+
+      // Flood Identification using NDFI
+      var ndfi = mean_before.abs().subtract(min_after.abs())
+        .divide(mean_before.abs()).add(min_after.abs());
+      var ndfi_filtered = ndfi.focal_mean({radius: 50, kernelType: 'circle', units: 'meters'});
+
+      // NDFI Normalization
+      var ndfi_min = ndfi_filtered.reduceRegion({
+      reducer: ee.Reducer.min(),
+      geometry: bounds,
+      scale: 10,
+      maxPixels: 1e13
+      });
+
+      var ndfi_max = ndfi_filtered.reduceRegion({
+      reducer: ee.Reducer.max(),
+      geometry: bounds,
+      scale: 10,
+      maxPixels: 1e13
+      });
+
+      var ndfi_rang = ee.Number(ndfi_max.get('VH')).subtract(ee.Number(ndfi_min.get('VH')));
+      var ndfi_subtctMin = ndfi_filtered.subtract(ee.Number(ndfi_min.get('VH')));
+      var ndfi_norm = ndfi_subtctMin.divide(ndfi_rang);
+
+      var histogram = ui.Chart.image.histogram({
+        image: ndfi_norm,
+        region: bounds,
+        scale: 10,
+        maxPixels: 1e13
+      });
+      // print("ndfi_norm Histogram", histogram);
+      
+      // NDFI Thresholding
+      var ndfi_mean = ndfi_norm.reduceRegion({
+      reducer: ee.Reducer.mean(),
+      geometry: bounds,
+      scale: 10,
+      maxPixels: 1e13
+      });
+      
+      var ndfi_std = ndfi_norm.reduceRegion({
+      reducer: ee.Reducer.stdDev(),
+      geometry: bounds,
+      scale: 10,
+      maxPixels: 1e13
+      });
+      
+      var ndfi_th = ee.Number(ndfi_mean.get('VH')).subtract(ee.Number(k_ndfi).multiply(ee.Number(ndfi_std.get('VH'))));
+      print('ndfi_th = ', ndfi_th);
+      
+      // Apply Thresholding Value on NDFI (lt is lower than)
+      var ndfi_filtered = ndfi_norm.lt(ndfi_th);
+      
+      
+      // NDFI Masking
+      var swater_mask = swater.gte(4);  //gte is greater than equal
+      var swater_clip = swater_mask.clip(bounds);
+      
+      var ndfi_flooded_masked = ndfi_filtered.where(swater_mask, 0);
+      var connections = ndfi_flooded_masked.connectedPixelCount().gte(25);
+      ndfi_flooded_masked = ndfi_flooded_masked.updateMask(connections.eq(1));
+      var ndfi_flood = ndfi_flooded_masked.updateMask(ndfi_flooded_masked.eq(1));
+      
+      // Visualizing result
+      // Map.addLayer(ndfi_flood, {palette: 'blue'}, 'ndfi_flood',0);
+      // Map.setOptions('HYBRID');
+      // Map.centerObject(bounds); 
     }
 
     // Display settings
